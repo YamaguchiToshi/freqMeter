c7b1671d7e7cef5852edfa260759791c39f402cf
Toshimitsu Yamaguchi <toshi.yamaguchi@eng.niigata-u.ac.jp>
2019-06-04 21:46:05 +0900

    続き

--- analyse/index.js
+++ analyse/index.js
@@ -60,7 +60,7 @@
         }
         ctx.rect(i * w, canvas.height - data[i] * 2, w, data[i] * 2);        
       }
-      if( maxVal > 50 ){
+      if( maxVal > 50 && maxIndex < 196 ){
         var maxFreq = maxIndex * 44100 / 2048;
         console.log('--->'+maxFreq);
       }
